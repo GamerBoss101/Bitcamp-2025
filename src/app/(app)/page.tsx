66fa4e8c8cffef0c72e2ed91592da4ce57ab3ebe
snekd <jolie.bella.bella@gmail.com>
2025-04-12 15:52:01 -0400

    h

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -8,6 +8,7 @@ function Mobile() {
 
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+			<img src="./drinkhappylogo.png" alt="Drink Happy Logo" className="row-start-1 h-10 sm:h-12 object-contain"/>
 			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
 				<h1 className="text-3xl sm:text-4xl font-bold tracking-[-.01em] text-center sm:text-left">
 					Welcome, {session?.name || "NULL"} !!
