bd74288c4f585ce6cc466f97362679138e34ab37
GamerBoss101 <adithcool.5@gmail.com>
2025-04-12 16:36:25 -0400

    NavBar Icons Update

--- src/lib/components/MobileNav.jsx
+++ src/lib/components/MobileNav.jsx
@@ -3,8 +3,9 @@
 import React from "react";
 import { Navigation } from "@skeletonlabs/skeleton-react";
 import {
+    Home as IconHome,
     Folder as IconFolder,
-    Image as IconImage,
+    BookText as BookImage,
     Music as IconMusic,
     Video as IconVideo,
 } from "lucide-react";
@@ -15,17 +16,11 @@ const Footer = () => {
     return (
         <div className="p-4 flex flex-col items-center gap-4 bg-surface-900">
             <Navigation.Bar className="flex justify-around w-full">
-                <Navigation.Tile label="Files" href="#/files" className="flex flex-col items-center">
-                    <IconFolder />
+                <Navigation.Tile label="Home" href="/" className="flex flex-col items-center">
+                    <IconHome />
                 </Navigation.Tile>
-                <Navigation.Tile label="Images" href="#/images" className="flex flex-col items-center">
-                    <IconImage />
-                </Navigation.Tile>
-                <Navigation.Tile label="Music" href="#/music" className="flex flex-col items-center">
-                    <IconMusic />
-                </Navigation.Tile>
-                <Navigation.Tile label="Videos" href="#/videos" className="flex flex-col items-center">
-                    <IconVideo />
+                <Navigation.Tile label="Info" href="/info" className="flex flex-col items-center">
+                    <BookImage />
                 </Navigation.Tile>
             </Navigation.Bar>
         </div>
